960c5ee20a11039d2af91a86876d05df4c1537d9
K B <k@km.com>
2019-03-18 21:33:30 -0400

    modified:   package.json 	modified:   server.js

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
+//NODE SERVER
+
+
+
 const express = require('express');
 const mongoose = require('mongoose');
 
 const bodyParser = require('body-parser');
+
+//core node js module
+const path = require('path');
+
+
 
 const spots = require('./routes/api/spots');
 const margins = require('./routes/api/margins');
@@ -29,6 +38,20 @@ mongoose
      app.use('/api/spots',spots);
      app.use('/api/margins',margins);
      app.use('/api/trades',trades);
+
+     //Serve static assets if in production
+     
+if(process.env.NODE_ENV === 'production'){
+
+  //"npm run build" in client or main folder to create/update build === static js
+  //setting static folder
+app.use(express.static('client/build'));
+
+app.get('*', ( req, res ) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 
     const port = process.env.PORT || 5000;
 
